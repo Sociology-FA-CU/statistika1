0fa40d9a8b341ba5d97e2fc5023ee58424fb0165
jaromir-mazak <mazak.jaromir@gmail.com>
2022-03-19 15:06:48 +0100

    preparations uploaded before lecture 5

---
+++ unused simpson.R
@@ -0,0 +1,42 @@
+## Je to pravda?
+
+<blockquote>
+  Vaccinated English adults under 60 are dying at twice the rate of unvaccinated people the same age.
+</blockquote>
+  
+  
+  Zdroj [ZDE](https://alexberenson.substack.com/p/vaccinated-english-adults-under-60)
+
+## Data jsou skutečná...
+
+Graf v článku vychází z dat zveřejněných ONS (Office for National Statistics, britský staťák).
+
+
+![](figures/05_vax_vs_unvax_rate.png){width=80%}
+
+## Jaké otázky bychom si mohli klást?
+
+> - Úmrtí v důsledku covidu, nebo obecně?
+  > - Věkový interval 10 až 59 let? Není to trochu moc?
+  > - Jak jsou očkovaní a neočkovaní rozloženi v tomto intervalu?
+  
+  ## Co se děje v agregovaných datech?
+  
+  <blockquote>
+  Professor Jeffrey Morris estimated that the annual mortality rate at the older end of the 10-59 age spectrum would be more than 50 times higher than the mortality rate at the younger end of the spectrum, with 478.2 per 100,000 deaths among 55-59 year olds and 8.8 per 100,000 among 10-14 year olds. [Reuters Fact Check](https://www.reuters.com/article/factcheck-coronavirus-britain-idUSL1N2SN1P4)
+</blockquote>
+  
+  
+  ## Verdikt REUTERS FACT CHECK
+  
+  <blockquote>
+  Zavádějící. Je pravda, že ve věkové skupině 10-59 let je vyšší míru úmrtnosti u očkovaných jedinců. Není to však důsledkem toho, že by očkování způsobovalo úmrtí, ale důsledkem vyšší míry očkování u starších věkových skupin v tomto širokém věkovém rozpětí, přičemž starší mají také vyšší úmrtnost.
+</blockquote>
+  
+  [Celý článek Reuters Fact Check](https://www.reuters.com/article/factcheck-coronavirus-britain-idUSL1N2SN1P4)
+
+## A jen smrti způsobené covidem?
+
+![](figures/05_covid_death_age.png){width=60%}
+
+[Celý článek BBC]("https://www.bbc.com/news/59757395")
